385e37745f05bd8832050d20e52b6bd462aed8bb
wpnhwq <52965395+wpnhwq@users.noreply.github.com>
2020-08-30 03:30:36 +0800

    提交 标题 ,java项目
    提交 标题 ,java项目  说明

---
+++ JavaProject/src/com/wpn/demo/Demo.java
@@ -0,0 +1,15 @@
+package com.wpn.demo;
+
+
+public class Demo {
+	
+	public void say() {
+		System.out.println("MavenAAriIDÏîÄ¿   package com.wpn.demo;  say()Ö´ÐÐ");
+		
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("AAA  XXX ");
+		
+	}
+}
